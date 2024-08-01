7035ba322340eb34e1b68909f3f515b79c75bf04
Peter Brohnst <126153672+Peter8790@users.noreply.github.com>
2024-08-01 16:11:39 +0200

    simple backend logic

---
+++ backend/dist/messages.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GAME_ERROR = exports.GAME_OVER = exports.MOVE = exports.NEW_GAME = exports.INIT_GAME = void 0;
+exports.INIT_GAME = "init_game";
+exports.NEW_GAME = "new_game";
+exports.MOVE = "move";
+exports.GAME_OVER = "game_over";
+exports.GAME_ERROR = "game_error";
---
+++ backend/src/messages.ts
@@ -0,0 +1,5 @@
+export const INIT_GAME = "init_game";
+export const NEW_GAME = "new_game";
+export const MOVE = "move";
+export const GAME_OVER = "game_over";
+export const GAME_ERROR = "game_error";
